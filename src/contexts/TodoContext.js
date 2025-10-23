c7de7491d889b257b12712e00ba9b8c7f3bf4a2f
Niraj-Kumar-Chauhan <nirajstudent74880@gmail.com>
2025-10-23 23:23:18 +0530

    Todo App

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext(
+    {
+        Todos: [
+            {
+                id: 1,
+                todo: "cricket",
+                completed: false
+            }
+        ],
+
+        addTodo: (todo) => {},
+        updateTodo: (id, todo) => {},
+        deleteTodo: (id) => {},
+        toggleComplete: (id) => {}
+    }
+)
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
---
+++ src/contexts/index.js
@@ -0,0 +1,5 @@
+import { TodoContext } from './TodoContext'
+import { TodoProvider } from './TodoContext'
+import { useTodo } from './TodoContext'
+
+export { TodoProvider, TodoContext, useTodo} from './TodoContext'
